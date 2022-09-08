998077ca898d8304bbbc7dc11521ebcf12daf2e6
vagrant <nabilahmed98@hotmail.com>
2022-09-08 23:07:05 +0000

    linking

--- express_server.js
+++ express_server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com" }
+
 app.set("view engine", "ejs");
 
 app.get("/urls", (req, res) => {
-  const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com" }
+  
   const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
     });
@@ -26,4 +28,9 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
 });
