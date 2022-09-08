c20cc6c9c93a750467f49ee4ff522e35be774884
vagrant <nabilahmed98@hotmail.com>
2022-09-08 22:37:47 +0000

    link

--- express_server.js
+++ express_server.js
@@ -4,10 +4,13 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+app.get("/urls", (req, res) => {
+  const urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com" }
+  const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+    });
 
 app.get("/", (req, res) => {
   res.send("Hello!");
