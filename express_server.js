37e6b5f794901fc41e8ce8e7a43d384772ec76a0
vagrant <nabilahmed98@hotmail.com>
2022-09-09 00:23:25 +0000

    shortening1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,13 @@
+function generateRandomString() {}
 const express = require("express");
 const app = express();
 const PORT = 8080;
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,6 +35,10 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
