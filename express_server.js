d0ae947d205fdd3740a0b4d057f95eb7cda0be2c
vagrant <nabilahmed98@hotmail.com>
2022-09-09 20:31:18 +0000

    shorturl

--- express_server.js
+++ express_server.js
@@ -1,17 +1,37 @@
-function generateRandomString() {}
+function generateRandomString() {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
 const express = require("express");
 const app = express();
 const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com" }
+  
+
+  
+  app.get("/u/:id", (req, res) => {
+    const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+  });
+  
+  app.post("/urls", (req, res) => {
+    console.log(req.body); 
+    const shortCode = generateRandomString();
+    const longUrl = req.body;
+    urlDatabase[shortCode] = longUrl;
+    res.send("Ok"); 
+});
+
 
 app.set("view engine", "ejs");
 
